5e657914c6cff51f30b66757b55874807f96a3aa
Skadoosh777 <670641576@qq.com>
2022-06-19 23:14:26 -0500

    Initial commit

---
+++ app/src/main/java/comp3350/calculator_android/presentation/MainActivity.java
@@ -0,0 +1,16 @@
+package comp3350.calculator_android.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import comp3350.calculator_android.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
