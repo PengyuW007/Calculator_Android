57fcd0d45a2fdad8433292bdfc149f611b267280
Skadoosh777 <670641576@qq.com>
2022-07-12 16:43:17 -0500

    MathParser added

--- app/src/main/java/comp3350/calculator_android/presentation/MainActivity.java
+++ app/src/main/java/comp3350/calculator_android/presentation/MainActivity.java
@@ -3,8 +3,13 @@ package comp3350.calculator_android.presentation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
 import android.widget.EditText;
 import android.view.View;
+
+import org.mariuszgromada.math.mxparser.*;
 
 import comp3350.calculator_android.R;
 
@@ -18,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         text = findViewById(R.id.textView2);
-        text.setShowSoftInputOnFocus(false);
+        //text.setShowSoftInputOnFocus(false);
+        text.setInputType(InputType.TYPE_NULL);
 
         text.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,7 +41,13 @@ public class MainActivity extends AppCompatActivity {
         int cur = text.getSelectionStart();
         String leftStr = oldStr.substring(0, cur);
         String rightStr = oldStr.substring(cur);
-        text.setText(String.format("%s%s%s", leftStr, newStr, rightStr));
+        //text.setText(String.format("%s%s%s", leftStr, newStr, rightStr));
+        if (getString(R.string.display).equals(text.getText().toString())) {
+            text.setText(newStr);
+        } else {
+            text.setText(leftStr + newStr + rightStr);
+        }
+        text.setSelection(cur + 1);
     }
 
 
@@ -71,7 +83,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void seven(View view) {
-
         updateText("7");
     }
 
@@ -87,60 +98,83 @@ public class MainActivity extends AppCompatActivity {
      * Functions
      ************/
     public void clear(View view) {
-updateText("");
+        text.setText("");
     }
 
     public void left_para(View view) {
-updateText("(");
+        updateText("(");
     }
 
     public void right_para(View view) {
+        updateText(")");
     }
 
     public void fractional(View view) {
-
+        updateText("!");
     }
 
     public void delete(View view) {
+        int curr = text.getSelectionStart();
+        int textLen = text.getText().length();
 
+        if (curr != 0 && textLen != 0) {
+            SpannableStringBuilder selection = (SpannableStringBuilder) text.getText();
+            selection.replace(curr - 1, curr, "");
+            text.setText(selection);
+            text.setSelection(curr - 1);
+        }
+
+//        int curr = text.getSelectionStart();
+//        int len = text.getText().length();
     }
 
     public void addition(View view) {
-
+        updateText("+");
     }
 
     public void subtraction(View view) {
-
+        updateText("-");
     }
 
     public void multiplication(View view) {
-
+        updateText("×");
     }
 
     public void division(View view) {
-
+        updateText("÷");
     }
 
     public void log(View view) {
-
+        updateText("㏒");
     }
 
     public void ln(View view) {
-
+        updateText("㏑");
     }
 
     public void power(View view) {
-
+        updateText("^");
     }
 
     public void decimal(View view) {
+        updateText(".");
     }
 
     public void percentage(View view) {
-
+        updateText("%");
     }
 
     public void equation(View view) {
+        String userExp = text.getText().toString();
 
+        userExp = userExp.replaceAll("÷","/");
+        userExp = userExp.replaceAll("×","*");
+
+        Expression expression = new Expression(userExp);
+
+        String result = String.valueOf(expression.calculate());
+
+        text.setText(result);
+        text.setSelection(result.length());
     }
 }
